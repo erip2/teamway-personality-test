c96af4444f3a1a1e40a5ab1e2082e4bd2185ef82
EriPanci <eri.panci@gmail.com>
2022-07-08 15:42:04 +0200

    💄 Added landing page, added images and new tailwind configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ['\'Red Hat Display\'', 'serif'],
+    },
+    extend: {
+      screens: {
+        '2xl': '1320px',
+      },
+    },
   },
   plugins: [],
 };
